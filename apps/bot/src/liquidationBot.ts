fad3403a449be28ebe57b477c87c606ca835b16b
Pedro Yves Fracari <yves.fracari@gmail.com>
2024-11-12 13:30:07 -0300

    chore: add monitor single position functions

--- apps/bot/src/liquidationBot.ts
+++ apps/bot/src/liquidationBot.ts
@@ -1,94 +1,180 @@
-import { PublicClient } from "viem";
-import { PikeClient } from "./transactions";
+import { Address, encodeFunctionData } from "viem";
+import { pTokenAbi } from "./abis/pTokenAbi";
+import { riskEngine } from "./utils/contracts";
+import { riskEngineAbi } from "./abis/riskEngineAbi";
+import { PikeClient, publicClient } from "./clients";
 
 export class LiquidationBot {
-  address: string;
-  transactionFactory: PikeClient;
-  liquidations: number = 0;
-  publicClient: PublicClient;
+  pikeClient: PikeClient;
   unwatchesFn: Record<string, () => void> = {};
+  blockNumber?: bigint = undefined;
 
-  constructor(
-    transactionFactory: PikeClient,
-    address: string,
-    publicClient: PublicClient
-  ) {
-    this.transactionFactory = transactionFactory;
-    this.address = address;
-    this.publicClient = publicClient;
+  constructor({ pikeClient }: { pikeClient: PikeClient }) {
+    this.pikeClient = pikeClient;
   }
 
-  // checkAndLiquidatePosition = async (
-  //   borrower: string,
-  //   borrowPToken: string,
-  //   collateralPToken: string
-  // ) => {
-  //   const amount = await this.transactionFactory.getCurrentBorrowAmount(
-  //     borrowPToken,
-  //     borrower
-  //   );
-  //   const amountToLiquidate = amount / 2n;
-  //   const canLiquidate = await this.transactionFactory.checkIfCanLiquidate(
-  //     borrowPToken,
-  //     borrower,
-  //     collateralPToken,
-  //     amountToLiquidate
-  //   );
-  //   if (canLiquidate) {
-  //     await this.liquidatePosition(
-  //       borrower,
-  //       borrowPToken,
-  //       amountToLiquidate,
-  //       collateralPToken
-  //     );
-  //     return true;
-  //   }
-  //   return false;
-  // };
+  setBlockNumber = (blockNumber: bigint) => {
+    this.blockNumber = blockNumber;
+  };
 
-  // startMonitorPosition = (
-  //   borrower: string,
-  //   pToken: string,
-  //   collateral: string
-  // ) => {
-  //   const unwatchesFn = this.publicClient.watchBlocks({
-  //     onBlock: (block) =>
-  //       this.checkAndLiquidatePosition(borrower, pToken, collateral),
-  //   });
-  //   this.unwatchesFn[borrower + pToken + collateral] = unwatchesFn;
-  // };
+  checkAndLiquidatePosition = async ({
+    borrowPToken,
+    borrower,
+    collateralPToken,
+  }: {
+    borrower: Address;
+    borrowPToken: Address;
+    collateralPToken: Address;
+  }) => {
+    const amountToLiquidate = await this.checkAmountToLiquidate({
+      borrowPToken,
+      borrower,
+      collateralPToken,
+    });
 
-  // stopMonitorPosition = (
-  //   borrower: string,
-  //   pToken: string,
-  //   collateral: string
-  // ) => {
-  //   this.unwatchesFn[borrower + pToken + collateral]();
-  // };
+    if (amountToLiquidate > 0n) {
+      await this.liquidatePosition({
+        borrower,
+        borrowPToken,
+        amountToLiquidate,
+        collateralPToken,
+      });
+      return true;
+    }
+    return false;
+  };
 
-  // liquidatePosition = async (
-  //   borrower: string,
-  //   borrowedPToken: string,
-  //   amount: bigint,
-  //   collateralPToken: string
-  // ) => {
-  //   await this.transactionFactory.mintToken(
-  //     getUnderlying(borrowedPToken),
-  //     this.address,
-  //     amount
-  //   );
-  //   await this.transactionFactory.approveToken(
-  //     getUnderlying(borrowedPToken),
-  //     this.address,
-  //     borrowedPToken,
-  //     amount
-  //   );
-  //   return this.transactionFactory.liquidateUser(
-  //     this.address,
-  //     borrower,
-  //     borrowedPToken,
-  //     amount,
-  //     collateralPToken
-  //   );
-  // };
+  checkAmountToLiquidate = async ({
+    borrowPToken,
+    borrower,
+    collateralPToken,
+  }: {
+    borrower: Address;
+    borrowPToken: Address;
+    collateralPToken: Address;
+  }) => {
+    const amount = await publicClient.readContract({
+      address: borrowPToken,
+      abi: pTokenAbi,
+      functionName: "borrowBalanceCurrent",
+      args: [borrower],
+    });
+
+    const amountToLiquidate = amount / 2n;
+
+    const liquidationAllowed = await this.checkLiquidationAllowed({
+      borrowPToken,
+      borrower,
+      collateralPToken,
+      amountToLiquidate,
+    });
+
+    return liquidationAllowed ? amountToLiquidate : 0n;
+  };
+
+  checkLiquidationAllowed = async ({
+    borrowPToken,
+    borrower,
+    collateralPToken,
+    amountToLiquidate,
+  }: {
+    borrower: Address;
+    borrowPToken: Address;
+    collateralPToken: Address;
+    amountToLiquidate: bigint;
+  }) => {
+    const liquidationErrorCode = await this.readContract({
+      address: riskEngine,
+      abi: riskEngineAbi,
+      functionName: "liquidateBorrowAllowed",
+      args: [borrowPToken, collateralPToken, borrower, amountToLiquidate],
+    });
+
+    return liquidationErrorCode == BigInt(0);
+  };
+
+  startMonitorPosition = ({
+    borrower,
+    borrowPToken,
+    collateralPToken,
+  }: {
+    borrower: Address;
+    borrowPToken: Address;
+    collateralPToken: Address;
+  }) => {
+    const unwatchesFn = publicClient.watchBlocks({
+      onBlock: (block) =>
+        this.checkAndLiquidatePosition({
+          borrower,
+          borrowPToken,
+          collateralPToken,
+        }),
+    });
+    this.unwatchesFn[borrower + borrowPToken + collateralPToken] = unwatchesFn;
+  };
+
+  stopMonitorPosition = ({
+    borrower,
+    borrowPToken,
+    collateralPToken,
+  }: {
+    borrower: Address;
+    borrowPToken: Address;
+    collateralPToken: Address;
+  }) => {
+    this.unwatchesFn[borrower + borrowPToken + collateralPToken]();
+  };
+
+  readContract = async ({
+    address,
+    abi,
+    functionName,
+    args,
+  }: {
+    address: Address;
+    abi: any;
+    functionName: string;
+    args: any[];
+  }) => {
+    return await publicClient.readContract({
+      address,
+      abi,
+      functionName,
+      args,
+      blockNumber: this.blockNumber,
+    });
+  };
+
+  liquidatePosition = async ({
+    borrowPToken,
+    borrower,
+    amountToLiquidate,
+    collateralPToken,
+  }: {
+    borrower: Address;
+    borrowPToken: Address;
+    amountToLiquidate: bigint;
+    collateralPToken: Address;
+  }) => {
+    // TODO: Implement liquidation
+    return;
+    // await this.pikeClient.mintToken(
+    //   getUnderlying(borrowedPToken),
+    //   this.address,
+    //   amount
+    // );
+    // await this.transactionFactory.approveToken(
+    //   getUnderlying(borrowedPToken),
+    //   this.address,
+    //   borrowedPToken,
+    //   amount
+    // );
+    // return this.transactionFactory.liquidateUser(
+    //   this.address,
+    //   borrower,
+    //   borrowedPToken,
+    //   amount,
+    //   collateralPToken
+    // );
+  };
 }
