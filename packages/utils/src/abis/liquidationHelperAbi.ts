eecd821206cfcef1d064d775d330b20cff66c105
Pedro Yves Fracari <yves.fracari@gmail.com>
2024-11-27 14:43:45 -0300

    chore: add new contracts abi

---
+++ packages/utils/src/abis/liquidationHelperAbi.ts
@@ -0,0 +1,92 @@
+export const liquidationHelperAbi = [
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "_uni_v3_factory",
+        type: "address",
+      },
+      {
+        internalType: "bytes",
+        name: "_pool_init_code_hash",
+        type: "bytes",
+      },
+    ],
+    stateMutability: "nonpayable",
+    type: "constructor",
+  },
+  {
+    inputs: [
+      {
+        internalType: "address",
+        name: "pool",
+        type: "address",
+      },
+      {
+        internalType: "IPToken",
+        name: "debtPToken",
+        type: "address",
+      },
+      {
+        internalType: "IPToken",
+        name: "collateralPToken",
+        type: "address",
+      },
+      {
+        internalType: "address",
+        name: "user",
+        type: "address",
+      },
+      {
+        internalType: "uint256",
+        name: "debtAmount",
+        type: "uint256",
+      },
+      {
+        internalType: "uint256",
+        name: "minOutputAmount",
+        type: "uint256",
+      },
+    ],
+    name: "liquidate",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+  {
+    inputs: [],
+    name: "uni_v3_factory",
+    outputs: [
+      {
+        internalType: "address",
+        name: "",
+        type: "address",
+      },
+    ],
+    stateMutability: "view",
+    type: "function",
+  },
+  {
+    inputs: [
+      {
+        internalType: "int256",
+        name: "amount0Delta",
+        type: "int256",
+      },
+      {
+        internalType: "int256",
+        name: "amount1Delta",
+        type: "int256",
+      },
+      {
+        internalType: "bytes",
+        name: "_data",
+        type: "bytes",
+      },
+    ],
+    name: "uniswapV3SwapCallback",
+    outputs: [],
+    stateMutability: "nonpayable",
+    type: "function",
+  },
+] as const;
